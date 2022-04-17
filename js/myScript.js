2732e2af86379d04f9dab25987390c6d8cb053be
Miss-Bamboo <yunqingy@andrew.cmu.edu>
2022-04-17 15:33:33 -0400

    picture randomization

--- js/myScript.js
+++ js/myScript.js
@@ -1,7 +1,39 @@
-window.onload = choosePic
+$(function() {
+window.onload = displayPic
 
-var myPix = new Array("img/portfolio/13.jpg","img/portfolio/14.jpg");
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
 
-function choosePic() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("myPicture").src = myPix[randomNum];
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+//Initialize an array of images
+var myPix = new Array();
+var prefix = "img/portfolio/";
+for (let i = 1; i <= 17; i++){
+  myPix.push(prefix.concat(i,".jpg"));
+}
+
+shuffle(myPix);
+
+function displayPic() {
+
+     var bar = document.getElementById("portfolio-sidebar").getElementsByTagName("li");
+     for (let i = 0; i < myPix.length; i++) {
+       bar[i].getElementsByTagName("img")[0].src = myPix[i];
+     }
+}
+
+})
